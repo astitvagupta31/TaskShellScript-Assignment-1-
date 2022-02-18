cdce87137a8c03f8c27963823f8bd6d0259244b7
Astitva Gupta <67801517+astitvagupta31@users.noreply.github.com>
2022-02-18 13:28:07 +0530

    File1

---
+++ scriptfile.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "Enter the user name: "  
+read first_name  
+echo "The Current User Name is $first_name"  
+echo  
+echo "Enter other users'names: "  
+read name1    
+echo "$name1 are the other users."  
+
